70ef0457a5ade57004911a10a18d22a83ebfac63
Prajwal Dl <dlprajwal183@gmail.com>
2025-07-08 10:19:56 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
-          <Toaster />
         </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   );
