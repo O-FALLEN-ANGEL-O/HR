1c47dbd1a3f1cce2437a22f4a3375f7fe5c4b6a4
Prajwal Dl <dlprajwal183@gmail.com>
2025-07-16 03:14:54 +0000

    https://www.behance.net/gallery/197239753/WorkVibe-SaaS-Art-Direction-UI

--- src/app/(app)/employee/kudos/page.tsx
+++ src/app/(app)/employee/kudos/page.tsx
@@ -56,7 +56,7 @@ export default async function KudosPage() {
 
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
-      <Header title="Kudos &amp; Recognition" />
+      <Header title="Kudos & Recognition" />
       <KudosClient
         currentUser={user}
         initialKudos={kudos}
