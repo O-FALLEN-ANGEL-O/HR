e7f9e5f019fbe7d708eaebb32581f84ab8d08ca4
Prajwal Dl <dlprajwal183@gmail.com>
2025-07-17 17:53:10 +0000

    As for the login issue there is no data in database as you given me data

---
+++ src/lib/supabase/handle_new_user.sql
@@ -0,0 +1,25 @@
+-- inserts a row into public.users
+create function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer set search_path = public
+as $$
+begin
+  insert into public.users (id, full_name, avatar_url, email, role, department, profile_setup_complete)
+  values (
+    new.id,
+    new.raw_user_meta_data ->> 'full_name',
+    new.raw_user_meta_data ->> 'avatar_url',
+    new.email,
+    coalesce((new.raw_user_meta_data ->> 'role'), 'guest')::user_role,
+    new.raw_user_meta_data ->> 'department',
+    false
+  );
+  return new;
+end;
+$$;
+
+-- trigger the function every time a user is created
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
