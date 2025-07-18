ff878177b2b1e4cc7eaaf519f3eb3954c55a05f0
Prajwal Dl <dlprajwal183@gmail.com>
2025-07-18 08:00:02 +0000

    In register page the webcam option to upload resume make the camera back

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -115,7 +115,7 @@ export default function RegisterPage() {
     if (showCamera) {
       const getCameraPermission = async () => {
         try {
-          const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+          const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' } });
           setHasCameraPermission(true);
           if (videoRef.current) {
             videoRef.current.srcObject = stream;
